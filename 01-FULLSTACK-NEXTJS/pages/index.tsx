ea2c0bdde8bad9cdb05862b255d9171452ac7eab
guilherme <guilherrmetwister@gmail.com>
2025-03-27 17:35:38 -0300

    refactor: update import paths and clean up tsconfig.json

--- 01-FULLSTACK-NEXTJS/pages/index.tsx
+++ 01-FULLSTACK-NEXTJS/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Box from "../src/components/box";
-import theme from "../src/theme/theme";
+import Box from "@src/components/box";
+import theme from "@src/theme/theme";
 
 export default function HomeScrean() {
   return (
