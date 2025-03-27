44b661e2eb0791e6d613d19d9669ff69fada738d
guilherme <guilherrmetwister@gmail.com>
2025-03-27 14:47:59 -0300

    feat: initial commit

---
+++ 01-FULLSTACK-NEXTJS/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Box from "../src/components/box";
+import theme from "../src/theme/theme";
+
+export default function HomeScrean() {
+  return (
+    <Box tag="main" styleSheet={{ fontFamily: theme.typografy.fontFamily }}>
+      ola
+    </Box>
+  );
+}
---
+++ 01-FULLSTACK-NEXTJS/src/theme/theme.ts
@@ -0,0 +1,6 @@
+const theme = {
+  typografy: {
+    fontFamily: '"Open Sans"," sans-serif"',
+  },
+};
+export default theme;
